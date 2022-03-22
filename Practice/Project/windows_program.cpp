9d785955571e43b678f113fdea7eadf0a38f1afe
SKYooon <ucs0191@naver.com>
2022-03-23 06:13:09 +0900

    P2-2 random color

--- Practice/Project/windows_program.cpp
+++ Practice/Project/windows_program.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <tchar.h>
+#include <random>
 
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = L"Window Class Name";
@@ -56,16 +57,27 @@ LRESULT CALLBACK wndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	RECT rect2{ 640, 0, 1280, 400 };
 	RECT rect3{ 0, 400, 640, 800 };
 	RECT rect4{ 640, 400, 1280, 800 };
+	std::default_random_engine dre;
+	std::uniform_int_distribution<int> uid;
 
 	switch (iMessage) {
 	case WM_PAINT:
 		hDC = BeginPaint(hWnd, &ps);
 
-		SetBkColor(hDC, RGB(0, 0, 0));
-		SetTextColor(hDC, RGB(255, 255, 255));
+		SetBkColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
+		SetTextColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
 		DrawText(hDC, L"This is left-top area", strlen("This is left-top area"), &rect1, DT_VCENTER | DT_CENTER | DT_SINGLELINE);
+
+		SetBkColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
+		SetTextColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
 		DrawText(hDC, L"This is right-top area", strlen("This is right-top area"), &rect2, DT_VCENTER | DT_CENTER | DT_SINGLELINE);
+
+		SetBkColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
+		SetTextColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
 		DrawText(hDC, L"This is left-bottom area", strlen("This is left-bottom area"), &rect3, DT_VCENTER | DT_CENTER | DT_SINGLELINE);
+
+		SetBkColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
+		SetTextColor(hDC, RGB(uid(dre), uid(dre), uid(dre)));
 		DrawText(hDC, L"This is right-bottom area", strlen("This is right-bottom area"), &rect4, DT_VCENTER | DT_CENTER | DT_SINGLELINE);
 
 		EndPaint(hWnd, &ps);
